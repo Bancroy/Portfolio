9868b6aa6f201c222b5d8d0afa2ff664417e997a
Bancroy <bancroy@gmail.com>
2016-04-10 23:54:53 +0200

    Home section separator

--- content/home.php
+++ content/home.php
@@ -1,7 +1,10 @@
 <section id="home" class="cuts-bottom">
     <div class="dim"></div>
-    <h1 class="font-roboto">
-        <small>Front-End</small>
-        Web Developer
-    </h1>
+    <div class="wrapper">
+        <h1 class="font-roboto">
+            <small>Front-End</small>
+            Web Developer
+        </h1>
+        <div class="separator"><span></span></div>
+    </div>
 </section>
