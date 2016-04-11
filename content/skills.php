c9d5b50503ab06cbec02b8b33c7b339c84156c03
Bancroy <bancroy@gmail.com>
2016-04-11 23:54:51 +0200

    General headings style for content sections

--- content/skills.php
+++ content/skills.php
@@ -1,5 +1,11 @@
 <section id="skills" class="content-section cuts-bottom cuts-top">
-    <div class="dim"></div>
     <h2 class="outline-only">Skills</h2>
+    <div class="dim"></div>
+    <header class="font-roboto">
+        <h3>
+            <small>Showtime!</small>
+            What I know
+        </h3>
+    </header>
     <div class="container"></div>
 </section>
