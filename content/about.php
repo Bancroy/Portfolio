c6b3f5ec81b08d0a85361c60504c4abc085fcb3e
Bancroy <bancroy@gmail.com>
2016-04-11 02:35:18 +0200

    About section

---
+++ content/about.php
@@ -0,0 +1,55 @@
+<section id="about">
+    <h2 class="outline-only">About</h2>
+    <div class="container">
+        <div class="row">
+            <div class="column xs-14 offset-xs-1 offset-sm-1 offset-md-1 lg-5 person">
+                <img src="/images/other/about-photo.png" alt="James Pietras">
+                <a class="font-lato" href="#">Resume</a>
+            </div>
+            <div class="column xs-14 offset-xs-1 offset-sm-1 offset-md-1 lg-10 offset-lg-1 offset-xl-1 row font-lato entries">
+                <article class="column xs-16 md-8">
+                    <header>
+                        <img src="/images/icons/about-me.png" alt="">
+                        <h3>Me</h3>
+                    </header>
+                    <p>I am young <strong>front-end web developer</strong> from Poland, charging his way through the job market. I work mostly with front-end, but challenges are always welcome.</p>
+                </article>
+                <article class="column xs-16 md-8">
+                    <header>
+                        <img src="/images/icons/about-contact.png" alt="">
+                        <h3>Contact</h3>
+                    </header>
+                    <p>Don't hesitate to contact me if you are interested in <strong>cooperation</strong>. I am in for almost any project. Leave your front-end development to me.</p>
+                </article>
+                <article class="column xs-16 md-8">
+                    <header>
+                        <img src="/images/icons/about-code.png" alt="">
+                        <h3>Code</h3>
+                    </header>
+                    <p>My highest priority is <strong>quality</strong>. Keeping code standarized, clean, short and functional is one of habits I stubbronly struggle to maintain.</p>
+                </article>
+                <article class="column xs-16 md-8">
+                    <header>
+                        <img src="/images/icons/about-hobbies.png" alt="">
+                        <h3>Hobbies</h3>
+                    </header>
+                    <p>When i am not smashing my keyboard, I <strong>exercise</strong> or devour piles of pizza. I can actually transform food into code. Professional front-end bias.</p>
+                </article>
+                <article class="column xs-16 md-8">
+                    <header>
+                        <img src="/images/icons/about-business.png" alt="">
+                        <h3>Business</h3>
+                    </header>
+                    <p>If you need <strong>stuff done right</strong>, hire me. My services are professional and reliable. Project, budget, deadline - everything is precisely described and fulfilled from top to bottom.</p>
+                </article>
+                <article class="column xs-16 md-8">
+                    <header>
+                        <img src="/images/icons/about-motivation.png" alt="">
+                        <h3>Motivation</h3>
+                    </header>
+                    <p>I spend my days <strong>crafting with passion</strong>. Everthing can become my digital playground, resulting in the most beatuiful and fun sandcastles on this worldwide beach.</p>
+                </article>
+            </div>
+        </div>
+    </div>
+</section>
