cf11231d6a7255a80e2ea58cd9ccea7ee098e70d
Bancroy <bancroy@gmail.com>
2016-04-13 17:41:12 +0200

    Skills section no-script technologies box

--- content/skills.php
+++ content/skills.php
@@ -7,5 +7,55 @@
             What I know
         </h3>
     </header>
-    <div class="container"></div>
+    <div class="container">
+        <section class="row">
+            <h3 class="outline-only">Technologies</h3>
+            <ul class="column xs-10 offset-xs-3 sm-8 offset-sm-4 md-6 offset-md-5 lg-4 offset-lg-6 offset-xl-6 font-lato results">
+                <li>
+                    <article>
+                        <header>
+                            <img src="/images/icons/skills-sass.png" alt="">
+                            <div class="wrapper">
+                                <h4>SASS</h4>
+                                <p class="explanation">Stylesheet preprocessor</p>
+                            </div>
+                        </header>
+                        <p class="description">
+                            This is the base of every single project I create.
+                            <abbr title="Syntactically Awesome Style Sheets">SASS</abbr> allows me to write 60-70% less code, while making it thrice as maintainable.
+                            I am very familiar with this technology and the way to use it.
+                        </p>
+                        <footer>
+                            <p class="familiarity">Familiarity:</p>
+                            <div role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="95" class="skillbar">
+                                <span style="width: 95%;"></span>
+                            </div>
+                        </footer>
+                    </article>
+                </li>
+                <li>
+                    <article>
+                        <header>
+                            <img src="/images/icons/skills-git.png" alt="">
+                            <div class="wrapper">
+                                <h4>Git</h4>
+                                <p class="explanation">Version control system</p>
+                            </div>
+                        </header>
+                        <p class="description">
+                            Git is the most popular <abbr title="Version Control System">VCS</abbr> used in almost every company.
+                            My public repositories are linked in "Home" section.
+                            You are welcome to browse them and judge quality of my code.
+                        </p>
+                        <footer>
+                            <p class="familiarity">Familiarity:</p>
+                            <div role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="70" class="skillbar">
+                                <span style="width: 70%;"></span>
+                            </div>
+                        </footer>
+                    </article>
+                </li>
+            </ul>
+        </section>
+    </div>
 </section>
