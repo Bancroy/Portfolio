5e217abcdc9f811f4a8815942c96a87bc257d1ba
Bancroy <bancroy@gmail.com>
2016-04-16 13:34:41 +0200

    About section rework

--- content/about.php
+++ content/about.php
@@ -1,39 +1,41 @@
 <section id="about" class="content-section">
     <h2 class="outline-only">About</h2>
     <div class="container">
-        <div class="row">
-            <div class="column xs-14 offset-xs-1 offset-sm-1 offset-md-1 lg-5 person">
-                <img src="/images/other/about-photo.png" alt="James Pietras">
-                <a class="font-lato" href="/images/other/about-resume.png" target="_blank" type="image/png">Resume</a>
-            </div>
-            <div class="column xs-14 offset-xs-1 offset-sm-1 offset-md-1 lg-10 offset-lg-1 offset-xl-1 row font-lato entries">
-                <article class="column xs-16 md-8">
+        <div class="row font-lato">
+            <div class="column xs-14 offset-xs-1 sm-10 offset-sm-3 md-8 offset-md-4 lg-5 entries-left">
+                <article>
                     <header>
                         <img src="/images/icons/about-me.png" alt="">
                         <h3>Me</h3>
                     </header>
                     <p>I am young <strong>front-end web developer</strong> from Poland, charging his way through the job market. I work mostly with front-end, but challenges are always welcome.</p>
                 </article>
-                <article class="column xs-16 md-8">
+                <article>
                     <header>
                         <img src="/images/icons/about-contact.png" alt="">
                         <h3>Contact</h3>
                     </header>
                     <p>Don't hesitate to contact me if you are interested in <strong>cooperation</strong>. I am in for almost any project. Leave your front-end development to me.</p>
                 </article>
+            </div>
+            <div class="column xs-14 offset-xs-1 offset-sm-1 offset-md-1 lg-4 offset-lg-1 offset-xl-1 person">
+                <img src="/images/other/about-photo.png" alt="James Pietras">
+                <a class="font-lato" href="/images/other/about-resume.png" target="_blank" type="image/png">Resume</a>
+            </div>
+            <div class="column xs-14 offset-xs-1 sm-10 offset-sm-3 md-8 offset-md-4 lg-5 offset-lg-1 offset-xl-1 entries-right">
                 <article class="column xs-16 md-8">
                     <header>
                         <img src="/images/icons/about-code.png" alt="">
                         <h3>Code</h3>
                     </header>
-                    <p>My highest priority is <strong>quality</strong>. Keeping code standarized, clean, short and functional is one of habits I stubbronly struggle to maintain.</p>
+                    <p>My highest priority is <strong>quality</strong>. Keeping code standarized, clean, short and functional is one of habits I stubbronly struggle to maintain. This is very important for front-end developer.</p>
                 </article>
                 <article class="column xs-16 md-8">
                     <header>
                         <img src="/images/icons/about-business.png" alt="">
                         <h3>Business</h3>
                     </header>
-                    <p>If you need <strong>stuff done right</strong>, hire me. My services are professional and reliable. Project, budget, deadline - everything is precisely described and fulfilled from top to bottom.</p>
+                    <p>If you need <strong>stuff done right</strong>, hire me. My services are professional and reliable. Project, budget, deadline - everything is precisely described and fulfilled.</p>
                 </article>
             </div>
         </div>
