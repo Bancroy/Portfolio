a0f357794e9ae5d84ae2ed68efa1f01812bbb041
Bancroy <bancroy@gmail.com>
2016-04-17 20:41:54 +0200

    Block mouse scroll and anchor pointer-events on smooth scroll

--- scripts/sticky-nav.js
+++ scripts/sticky-nav.js
@@ -1,13 +1,24 @@
 "use strict";
 
+function blockScroll() {
+    return false;
+}
+
 $(document).ready(function stickyNav() {
     var nav = $("nav");
     var nav_breakpoint = nav.offset().top + nav.height();
 
     nav.find("a").on("click", function smoothAnchorScroll() {
+        var nav_links = $("nav li a");
+        $(document).on("mousewheel DOMMouseScroll", blockScroll);
+        nav_links.css("pointer-events", "none");
+
         $("html, body").animate({
             scrollTop: $($(this).attr("href")).offset().top
-        }, 1000);
+        }, 1000, function unblock() {
+            $(document).off("mousewheel DOMMouseScroll", blockScroll);
+            nav_links.css("pointer-events", "all");
+        });
 
         return false;
     });
