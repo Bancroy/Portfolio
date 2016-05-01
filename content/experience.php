aff5b0730370fd55861c8e3358a83b9b2af71e9a
Bancroy <bancroy@gmail.com>
2016-05-01 02:32:43 +0200

    Outline corrections

--- content/experience.php
+++ content/experience.php
@@ -7,15 +7,16 @@
         </p>
     </header>
     <div class="container row">
-        <div class="column xs-16 font-lato timeline">
+        <section class="column xs-16 font-lato timeline">
+            <h3 class="outline-only">Timeline</h3>
             <time class="year"><?php echo date("Y"); ?></time>
             <ul>
                 <li>
                     <img src="/images/icons/experience-work.png" alt="">
                     <article>
                         <p class="position">Wordpress Developer</p>
-                        <h3 class="company"><a href="http://ai7.pl/">AI7</a></h3>
-                        <time class="period">April 2016 - Present</time>
+                        <h4 class="company"><a href="http://ai7.pl/">AI7</a></h4>
+                        <div class="period">April 2016 - Present</div>
                         <p class="description">
                             Long term cooperation as a subcontractor.
                             My responsibility was to create whole websites based on Wordpress CMS.
@@ -27,8 +28,8 @@
                     <img src="/images/icons/experience-work.png" alt="">
                     <article>
                         <p class="position">Front-End Web Developer</p>
-                        <h3 class="company"><a href="https://go4sport.pl/">Go4sport</a></h3>
-                        <time class="period">March 2016 - Present</time>
+                        <h4 class="company"><a href="https://go4sport.pl/">Go4sport</a></h4>
+                        <div class="period">March 2016 - Present</div>
                         <p class="description">
                             Hired as the only front-end developer in the team.
                             I was creating advanced, responsive layouts for the whole service.
@@ -40,8 +41,8 @@
                     <img src="/images/icons/experience-intern.png" alt="">
                     <article>
                         <p class="position">Junior Web Developer</p>
-                        <h3 class="company"><a href="http://ivision.pl/">Ivision.pl</a></h3>
-                        <time class="period">July 2015</time>
+                        <h4 class="company"><a href="http://ivision.pl/">Ivision.pl</a></h4>
+                        <div class="period">July 2015</div>
                         <p class="description">
                             Short term intern where I was required to work with both front-end and back-end.
                             Most of my tasks were special training projects or corrections in products.
