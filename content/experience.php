78907624d89ff2c15b2404e54fc30d45d88b92cd
Bancroy <bancroy@gmail.com>
2016-05-01 02:27:17 +0200

    Experience section timeline

--- content/experience.php
+++ content/experience.php
@@ -6,5 +6,51 @@
             Experience
         </p>
     </header>
-    <div class="container"></div>
+    <div class="container row">
+        <div class="column xs-16 font-lato timeline">
+            <time class="year"><?php echo date("Y"); ?></time>
+            <ul>
+                <li>
+                    <img src="/images/icons/experience-work.png" alt="">
+                    <article>
+                        <p class="position">Wordpress Developer</p>
+                        <h3 class="company"><a href="http://ai7.pl/">AI7</a></h3>
+                        <time class="period">April 2016 - Present</time>
+                        <p class="description">
+                            Long term cooperation as a subcontractor.
+                            My responsibility was to create whole websites based on Wordpress CMS.
+                            All the work was done on my customized theme.
+                        </p>
+                    </article>
+                </li>
+                <li>
+                    <img src="/images/icons/experience-work.png" alt="">
+                    <article>
+                        <p class="position">Front-End Web Developer</p>
+                        <h3 class="company"><a href="https://go4sport.pl/">Go4sport</a></h3>
+                        <time class="period">March 2016 - Present</time>
+                        <p class="description">
+                            Hired as the only front-end developer in the team.
+                            I was creating advanced, responsive layouts for the whole service.
+                            My views were attached to Yii framework and used by large audience.
+                        </p>
+                    </article>
+                </li>
+                <li>
+                    <img src="/images/icons/experience-intern.png" alt="">
+                    <article>
+                        <p class="position">Junior Web Developer</p>
+                        <h3 class="company"><a href="http://ivision.pl/">Ivision.pl</a></h3>
+                        <time class="period">July 2015</time>
+                        <p class="description">
+                            Short term intern where I was required to work with both front-end and back-end.
+                            Most of my tasks were special training projects or corrections in products.
+                            I was also working quite a lot with wordpress customization.
+                        </p>
+                    </article>
+                </li>
+            </ul>
+            <time class="year">2015</time>
+        </div>
+    </div>
 </section>
