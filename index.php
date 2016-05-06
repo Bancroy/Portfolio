6fa126ca27322188bd2ba7431bec8e728675d0f0
Bancroy <bancroy@gmail.com>
2016-05-06 23:09:26 +0200

    Favicon

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="James Pietras is a freelance, professional front-end web developer. Providing quality user experience through responsive design and performant front-end developement.">
         <meta name="keywords" content="front-end developer, front-end developement, web developer, portfolio, james pietras">
+        <link rel="icon" href="/images/favicon.ico" type="image/x-icon">
         <title>Portfolio | James Pietras - Front-End Web Developer</title>
         <link rel="stylesheet" href="style.css">
     </head>
